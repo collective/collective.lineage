055695446c2ce2b73d016884e8ae5b66644758ff
lucie <lucie@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-04-30 13:12:56 +0000

    bumping version number after release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 from setuptools import setup, find_packages
 
-version = '0.4dev'
+version = '0.5dev'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
