4ca0f24f4cc3eaecec685b0566ecc988c5b9dd5e
lucie <lucie@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-01-12 23:14:12 +0000

    merging the v0.6migrationfixes branch back to the trunk

--- src/collective/lineage/tests.py
+++ src/collective/lineage/tests.py
@@ -1,6 +1,7 @@
 import zope.interface
 import zope.component
 from AccessControl import Unauthorized
+from plone.app.layout.navigation.interfaces import INavigationRoot
 
 #for plone3-plone4 compatibility purposes
 try:
@@ -12,11 +13,21 @@ try:
 except:
     import zope.annotation.interfaces as zai
 
+from DateTime import DateTime
+from five.localsitemanager import make_objectmanager_site
 
 from p4a.subtyper import interfaces
 from p4a.subtyper import default
 from p4a.subtyper import engine
+from plone.app.layout.navigation.defaultpage import getDefaultPage
+from plone.portlets.constants import CONTEXT_CATEGORY, GROUP_CATEGORY, CONTENT_TYPE_CATEGORY
+from plone.portlets.interfaces import ILocalPortletAssignable
+from plone.portlets.interfaces import ILocalPortletAssignmentManager
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import IPortletType
+
 import Products.Archetypes.interfaces
+from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
 
 from zope.app.component.interfaces import ISite
 
@@ -32,6 +43,12 @@ from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import onsetup
 from Testing import ZopeTestCase as ztc
 
+from zope.app.component.interfaces import ISite
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+from zope.interface import alsoProvides
+from zope.interface import noLongerProvides
+
 @onsetup
 def setup_package():
     fiveconfigure.debug_mode = True
@@ -42,7 +59,6 @@ def setup_package():
 
 setup_package()
 ptc.setupPloneSite(products=['collective.lineage'])
-
 
 class IntegrationTests(ptc.PloneTestCase):
 
@@ -73,101 +89,6 @@ class IntegrationTests(ptc.PloneTestCase):
         subtyper.change_type(self.portal.site1, u'collective.lineage.childsite')
         self.failUnless(ISite.providedBy(self.portal.site1))
 
-    def test_migration(self):
-        """we are going to test the migration from 0.1 to >0.1"""
-        roles = ('Member', 'Manager')
-        self.portal.portal_membership.addMember('manager',
-                                                'secret',
-                                                roles, [])
-        self.login('manager')
-        pw = getToolByName(self.portal, "portal_workflow")
-
-        # allow the Child Folder type to be addable
-        pt = getToolByName(self.portal, "portal_types")
-        cf_type = pt["Child Folder"]
-        cf_type.global_allow = True
-
-        # cf1
-        self.portal.invokeFactory("Child Folder", "cf1")
-        cf1 = self.portal.cf1
-        cf1.setTitle("CF 1")
-        cf1.setDescription("Description of CF 1")
-        cf1.layout = "layout1"
-        pw.doActionFor(cf1, "publish")
-        self.failUnless(cf1.Title() == "CF 1")
-        self.failUnless(cf1.Description() == "Description of CF 1")
-        self.failUnless(pw.getInfoFor(cf1, "review_state") == "published")
-        self.failUnless(cf1.layout == "layout1")
-
-        cf1.invokeFactory("Document", "doc1")
-        cf1.invokeFactory("Document", "doc2")
-        doc1 = cf1["doc1"]
-        doc1.setTitle("Doc 1")
-        doc1.setDescription("Description of Doc 1")
-        doc1.setText("<p>Some Text here</p>")
-
-        # cf2
-        self.portal.invokeFactory("Child Folder", "cf2")
-        cf2 = self.portal.cf2
-
-
-        #cf3
-        cf2.invokeFactory("Child Folder", "cf3")
-        cf3 = cf2.cf3
-        cf3.setTitle("CF 3")
-        cf3.setDescription("Description of CF 3")
-        cf3.layout = "3layout"
-        pw.doActionFor(cf3, "publish")
-        self.failUnless(cf3.Title() == "CF 3")
-        self.failUnless(cf3.Description() == "Description of CF 3")
-        self.failUnless(pw.getInfoFor(cf3, "review_state") == "published")
-
-        cf3.invokeFactory("Document", "doc1")
-        cf3.invokeFactory("Document", "doc2")
-        doc1 = cf3["doc1"]
-        doc1.setTitle("Doc 1")
-        doc1.setDescription("Description of Doc 1")
-        doc1.setText("<p>Some Text here</p>")
-
-        import transaction; transaction.savepoint();
-        # Now run the migration step
-        profile_id = "collective.lineage:default"
-        setup_tool = getToolByName(self.portal, 'portal_setup')
-        steps_to_run = _upgrade_registry.getUpgradeStepsForProfile(profile_id)
-
-        for step_id in steps_to_run:
-            step = _upgrade_registry.getUpgradeStep(profile_id, step_id)
-            if step is not None:
-                if step.title == "Migrate the Child Folder objects":
-                    step.doStep(setup_tool)
-
-        # then we test if cf1-3 are still existing
-        # but are just normal folder that are subtyped
-        # also check they still have the correct title
-        # description, content, state and layout
-        cf1 = self.portal.cf1
-        cf2 = self.portal.cf2
-        cf3 = cf2.cf3
-        doc1 = cf3.doc1
-        self.failUnless(cf1.Title() == "CF 1")
-        self.failUnless(pw.getInfoFor(cf1, "review_state") == "published")
-        self.failUnless(cf1.layout == "layout1")
-        self.failUnless(cf3.Title() == "CF 3")
-        self.failUnless(cf3.Description() == "Description of CF 3")
-        self.failUnless(cf3.layout == "3layout")
-        self.failUnless(pw.getInfoFor(cf3, "review_state") == "published")
-        self.failUnless(cf1.portal_type == "Folder")
-        self.failUnless(cf3.portal_type == "Folder")
-        self.failUnless(doc1.getRawText() == "<p>Some Text here</p>")
-
-        #check that anonymous can see the published items
-        self.logout()
-        try:
-            cf1_item = self.portal.restrictedTraverse("cf1")
-        except Unauthorized:
-            cf1_item = None
-        self.failUnless(cf1_item != None)
-
     def test_uninstall(self):
         """testing the uninstall of collective.lineage"""
         roles = ('Member', 'Manager')
@@ -191,9 +112,220 @@ class IntegrationTests(ptc.PloneTestCase):
         self.failUnless(u'collective.lineage.childsite' not in \
                             dict(adapted.possible).keys())
 
+class MigrationTests(ptc.PloneTestCase):
+    """we are going to test the migration from 0.1 to >0.1"""
+
+    def afterSetUp(self):
+        roles = ('Member', 'Manager')
+        self.portal.portal_membership.addMember('manager',
+                                                'secret',
+                                                roles, [])
+        self.portal.portal_membership.addMember('testuser',
+                                                'secret',
+                                                (), [])
+        self.login('manager')
+
+        # allow the Child Folder type to be addable
+        pt = getToolByName(self.portal, "portal_types")
+        cf_type = pt["Child Folder"]
+        cf_type.global_allow = True
+
+        self.pw = getToolByName(self.portal, "portal_workflow")
+
+
+    def run_migration_step(self):
+        import transaction; transaction.savepoint();
+        # Now run the migration step
+        profile_id = "collective.lineage:default"
+        setup_tool = getToolByName(self.portal, 'portal_setup')
+        steps_to_run = _upgrade_registry.getUpgradeStepsForProfile(profile_id)
+        for step_id in steps_to_run:
+            step = _upgrade_registry.getUpgradeStep(profile_id, step_id)
+            if step is not None:
+                if step.title == "Migrate the Child Folder objects":
+                    step.doStep(setup_tool)
+
+    def test_migration(self):
+        # cf1
+        self.portal.invokeFactory("Child Folder", "cf1")
+        cf1 = self.portal.cf1
+        cf1.setTitle("CF 1")
+        cf1.setDescription("Description of CF 1")
+        cf1.layout = "layout1"
+        self.pw.doActionFor(cf1, "publish")
+        self.failUnless(cf1.Title() == "CF 1")
+        self.failUnless(cf1.Description() == "Description of CF 1")
+        self.failUnless(self.pw.getInfoFor(cf1, "review_state") == "published")
+        self.failUnless(cf1.layout == "layout1")
+
+        cf1.invokeFactory("Document", "doc1")
+        cf1.invokeFactory("Document", "doc2")
+        doc1 = cf1["doc1"]
+        doc1.setTitle("Doc 1")
+        doc1.setDescription("Description of Doc 1")
+        doc1.setText("<p>Some Text here</p>")
+
+        # cf2
+        self.portal.invokeFactory("Child Folder", "cf2")
+        cf2 = self.portal.cf2
+
+
+        #cf3
+        cf2.invokeFactory("Child Folder", "cf3")
+        cf3 = cf2.cf3
+        cf3.setTitle("CF 3")
+        cf3.setDescription("Description of CF 3")
+        cf3.layout = "3layout"
+        self.pw.doActionFor(cf3, "publish")
+        self.failUnless(cf3.Title() == "CF 3")
+        self.failUnless(cf3.Description() == "Description of CF 3")
+        self.failUnless(self.pw.getInfoFor(cf3, "review_state") == "published")
+
+        cf3.invokeFactory("Document", "doc1")
+        cf3.invokeFactory("Document", "doc2")
+        doc1 = cf3["doc1"]
+        doc1.setTitle("Doc 1")
+        doc1.setDescription("Description of Doc 1")
+        doc1.setText("<p>Some Text here</p>")
+
+        self.run_migration_step()
+
+        # then we test if cf1-3 are still existing
+        # but are just normal folder that are subtyped
+        # also check they still have the correct title
+        # description, content, state and layout
+        cf1 = self.portal.cf1
+        cf2 = self.portal.cf2
+        cf3 = cf2.cf3
+        doc1 = cf3.doc1
+        self.failUnless(cf1.Title() == "CF 1")
+        self.failUnless(self.pw.getInfoFor(cf1, "review_state") == "published")
+        self.failUnless(cf1.layout == "layout1")
+        self.failUnless(cf3.Title() == "CF 3")
+        self.failUnless(cf3.Description() == "Description of CF 3")
+        self.failUnless(cf3.layout == "3layout")
+        self.failUnless(self.pw.getInfoFor(cf3, "review_state") == "published")
+        self.failUnless(cf1.portal_type == "Folder")
+        self.failUnless(cf3.portal_type == "Folder")
+        self.failUnless(doc1.getRawText() == "<p>Some Text here</p>")
+
+        #check that anonymous can see the published items
+        self.logout()
+        try:
+            cf1_item = self.portal.restrictedTraverse("cf1")
+        except Unauthorized:
+            cf1_item = None
+        self.failUnless(cf1_item != None)
+
+    def test_migration_preserves_default_view(self):
+        self.portal.invokeFactory("Child Folder", "cf1")
+        cf1 = self.portal.cf1
+        cf1.setTitle("CF 1")
+        self.pw.doActionFor(cf1, "publish")
+        self.failUnless(cf1.Title() == "CF 1")
+        self.failUnless(self.pw.getInfoFor(cf1, "review_state") == "published")
+        cf1.invokeFactory("Document", "doc1")
+        doc1 = cf1["doc1"]
+        doc1.setTitle("Doc 1")
+        cf1.setDefaultPage("doc1")
+
+        self.run_migration_step()
+
+        cf1 = self.portal.cf1
+        self.assertEquals(cf1.getDefaultPage(), "doc1")
+
+    def test_migration_preserves_portlets(self):
+        self.portal.invokeFactory("Child Folder", "cf1")
+        cf1 = self.portal.cf1
+        cf1.setTitle("CF 1")
+        make_objectmanager_site(cf1)
+        self.pw.doActionFor(cf1, "publish")
+        self.failUnless(cf1.Title() == "CF 1")
+        self.failUnless(self.pw.getInfoFor(cf1, "review_state") == "published")
+        self.failUnless(ISite.providedBy(cf1))
+        # Child folders in 0.1 seemed to provide ILocalPortletAssignable but not in 0.6
+        if not ILocalPortletAssignable.providedBy(cf1):
+            alsoProvides(cf1, ILocalPortletAssignable)
+            added_portlet_assignable_interace = True
+        else:
+            added_portlet_assignable_interace = False
+
+        mapping = cf1.restrictedTraverse('++contextportlets++plone.leftcolumn')
+        for m in mapping.keys():
+            del mapping[m]
+        portlet = getUtility(IPortletType, name='plone.portlet.static.Static')
+        addview = mapping.restrictedTraverse('+/' + portlet.addview)
+        addview.createAndAdd(data={'header' : u"test title", 'text' : u"test text"})
+        self.assertEquals(len(mapping), 1)
+
+        left_col_manager = getUtility(IPortletManager, name='plone.leftcolumn', context=cf1)
+        assignment_manager = getMultiAdapter((cf1, left_col_manager), ILocalPortletAssignmentManager)
+        assignment_manager.setBlacklistStatus(GROUP_CATEGORY, None)
+        assignment_manager.setBlacklistStatus(CONTENT_TYPE_CATEGORY, False)
+
+        if added_portlet_assignable_interace:
+            noLongerProvides(cf1, ILocalPortletAssignable)
+
+        self.run_migration_step()
+
+        cf1 = self.portal.cf1
+        mapping = cf1.restrictedTraverse('++contextportlets++plone.leftcolumn')
+        self.assertEquals(len(mapping), 1)
+
+        left_col_manager = getUtility(IPortletManager, name='plone.leftcolumn', context=cf1)
+        assignment_manager = getMultiAdapter((cf1, left_col_manager), ILocalPortletAssignmentManager)
+        self.assertTrue(assignment_manager.getBlacklistStatus(CONTEXT_CATEGORY))
+        self.assertTrue(assignment_manager.getBlacklistStatus(GROUP_CATEGORY) is None)
+        self.assertFalse(assignment_manager.getBlacklistStatus(CONTENT_TYPE_CATEGORY))
+
+    def test_migration_preserves_references(self):
+        self.portal.invokeFactory("Child Folder", "cf1")
+        cf1 = self.portal.cf1
+        cf1.setTitle("CF 1")
+        make_objectmanager_site(cf1)
+        self.pw.doActionFor(cf1, "publish")
+        self.failUnless(cf1.Title() == "CF 1")
+        self.failUnless(self.pw.getInfoFor(cf1, "review_state") == "published")
+        self.failUnless(ISite.providedBy(cf1))
+
+        cf1.invokeFactory("Document", "doc1", Title="Doc 1")
+        doc1 = cf1["doc1"]
+        cf1.setDefaultPage("doc1")
+
+        doc2_text = '<p><a href="resolveuid/%s" class="internal">Link to doc 1</a></p>' % doc1.UID()
+        cf1.invokeFactory("Document", "doc2", Title="Doc 2", text=doc2_text)
+        doc2 = cf1["doc2"]
+        # I'm not sure what layer of Plone code adds these
+        doc2.addReference(doc1, 'isReferencing', updateReferences=True)
+        self.assertEquals(len(doc2._getReferenceAnnotations().objectItems()), 1)
+
+        self.run_migration_step()
+
+        cf1 = self.portal.cf1
+        doc2 = cf1["doc2"]
+        self.assertEquals(len(doc2._getReferenceAnnotations().objectItems()), 1)
+
+
+    def test_migration_preserves_sharing_settings(self):
+        self.portal.invokeFactory("Child Folder", "cf1")
+        cf1 = self.portal.cf1
+        cf1.setTitle("CF 1")
+        make_objectmanager_site(cf1)
+        self.pw.doActionFor(cf1, "publish")
+        self.failUnless(cf1.Title() == "CF 1")
+        self.failUnless(self.pw.getInfoFor(cf1, "review_state") == "published")
+        self.failUnless(ISite.providedBy(cf1))
+
+        cf1.manage_setLocalRoles('testuser', ['Contributor'])
+
+        self.run_migration_step()
+
+        cf1 = self.portal.cf1
+        self.assertEquals(('Contributor',), cf1.get_local_roles_for_userid('testuser'))
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(IntegrationTests))
+    suite.addTest(unittest.makeSuite(MigrationTests))
     return suite
 
--- src/collective/lineage/upgrades.py
+++ src/collective/lineage/upgrades.py
@@ -1,15 +1,36 @@
-from Products.CMFCore.utils import getToolByName
 from DateTime import DateTime
 
-import zope.component
-import transaction
+from logging import getLogger
 
 from p4a.subtyper import engine
 from p4a.subtyper import interfaces
 
+from plone.app.layout.navigation.defaultpage import getDefaultPage
+from plone.portlets.interfaces import ILocalPortletAssignable
+from plone.portlets.interfaces import ILocalPortletAssignmentManager
+from plone.portlets.interfaces import IPortletAssignmentMapping
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.constants import CONTEXT_CATEGORY
+from plone.portlets.constants import GROUP_CATEGORY
+from plone.portlets.constants import CONTENT_TYPE_CATEGORY
+
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.utils import base_hasattr
+
+from zope.interface import alsoProvides
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+from zope.component import getUtilitiesFor
+from zope.component import queryUtility
+from zope.component import provideUtility
+import transaction
+
+logger = getLogger('collective.lineage.upgrades')
+
 def migrateChildFolders(context):
     """Migrates Child Folder objects to normal Folder objects
     that are subtyped"""
+    logger.info('Migrating child folder objects to normal folder objects')
     # allow the Child Folder type to be addable
     pt = getToolByName(context, "portal_types")
     pw = getToolByName(context, "portal_workflow")
@@ -20,9 +41,8 @@ def migrateChildFolders(context):
     brains = pc.searchResults(portal_type="Child Folder")
     while brains:
         child_folder = brains[0].getObject()
+        logger.info('Migrating child folder %s' % child_folder.getId())
         parent = child_folder.getParentNode()
-        children_ids = child_folder.objectIds()
-
         # we will keep the same state, title, description, id
         # and display
         cf_state = pw.getInfoFor(child_folder, "review_state")
@@ -30,22 +50,22 @@ def migrateChildFolders(context):
         cf_wf = cf_wf and cf_wf[0]
         cf_title = child_folder.Title()
         cf_desc = child_folder.Description()
-        cf_id = child_folder.getId()
-        has_layout = hasattr(child_folder, "layout")
+        cf_orig_id = child_folder.getId()
+        has_layout = base_hasattr(child_folder, "layout")
         if has_layout:
             cf_display = child_folder.layout
-        cf_new_id = "%s-old" % cf_id
-        parent.manage_renameObjects([cf_id], [cf_new_id])
+        cf_default_page = getDefaultPage(child_folder)
+        f_temp_id = '%s-temp' % cf_orig_id
 
-        parent.invokeFactory("Folder", cf_id)
-        new_folder = parent[cf_id]
+        parent.invokeFactory("Folder", f_temp_id)
+        new_folder = parent[f_temp_id]
         new_folder.setTitle(cf_title)
         new_folder.setDescription(cf_desc)
         if has_layout:
             new_folder.layout = cf_display
         new_folder.processForm()
-        zope.component.provideUtility(engine.Subtyper())
-        subtyper = zope.component.getUtility(interfaces.ISubtyper)
+        provideUtility(engine.Subtyper())
+        subtyper = getUtility(interfaces.ISubtyper)
         subtyper.change_type(new_folder, u'collective.lineage.childsite')
 
         if cf_wf:
@@ -59,16 +79,69 @@ def migrateChildFolders(context):
             pw.setStatusOf(cf_wf, new_folder, new_state)
             new_folder.reindexObject()
 
-
+        children_ids = child_folder.objectIds()
         if children_ids:
             cut_items = child_folder.manage_cutObjects(ids=children_ids)
             new_folder.manage_pasteObjects(cut_items)
+        if cf_default_page:
+            new_folder.setDefaultPage(cf_default_page)
+        copy_portlet_assignments_and_settings(child_folder, new_folder)
+        copy_sharing_settings(child_folder, new_folder)
 
-        parent.manage_delObjects([cf_new_id])
+        parent.manage_delObjects([cf_orig_id])
         transaction.savepoint()
-
+        parent.manage_renameObjects([f_temp_id], [cf_orig_id])
+        transaction.savepoint()
         brains = pc.searchResults(portal_type="Child Folder")
 
     pw.updateRoleMappings()
     cf_type.global_allow = False
+    logger.info('Finished migrating child folders')
+
+def copy_sharing_settings(src, target):
+    # copying the sharing roles
+    sharing_view = getMultiAdapter((src, src.REQUEST), name="sharing")
+    sharing_settings = sharing_view.existing_role_settings()
+    for user_sharing_setting in sharing_settings:
+        # may equally be a group but doesn't matter
+        user_id = user_sharing_setting['id']
+        user_roles_set_here = [role_id
+                        for role_id, explicitly_set in \
+                               user_sharing_setting['roles'].items()
+                        if explicitly_set]
+        if user_roles_set_here:
+            target.manage_setLocalRoles(user_id, user_roles_set_here)
+    # copying the inheritance
+    target_sharing_view = getMultiAdapter((target, target.REQUEST),
+                                           name="sharing")
+    target_sharing_view.update_inherit(sharing_view.inherited())
+
+
+def copy_portlet_assignments_and_settings(src, target):
+    if not ILocalPortletAssignable.providedBy(src):
+        alsoProvides(src, ILocalPortletAssignable)
+
+    for manager_name, src_manager in getUtilitiesFor(IPortletManager, context=src):
+        src_manager_assignments = getMultiAdapter((src, src_manager), IPortletAssignmentMapping)
+        target_manager = queryUtility(IPortletManager, name=manager_name, context=target)
+        if target_manager is None:
+            logger.warning('New folder %s does not have portlet manager %s' %
+                           (target.getId(), target_manager))
+        else:
+            target_manager_assignments = getMultiAdapter((target, target_manager),
+                                                IPortletAssignmentMapping)
+            for id, assignment in src_manager_assignments.items():
+                target_manager_assignments[id] = assignment
+            src_assignment_manager = getMultiAdapter((src, src_manager),
+                                                 ILocalPortletAssignmentManager)
+            target_assignment_manager = getMultiAdapter((target, target_manager),
+                                                 ILocalPortletAssignmentManager)
+            #
+            # In lineage 0.1 child folders did not inherit their parent's portlets
+            # no matter what porlet block settings were set.
+            #
+            target_assignment_manager.setBlacklistStatus(CONTEXT_CATEGORY, True)
+            for category in (GROUP_CATEGORY, CONTENT_TYPE_CATEGORY):
+                target_assignment_manager.setBlacklistStatus(category,
+                                      src_assignment_manager.getBlacklistStatus(category))
 
