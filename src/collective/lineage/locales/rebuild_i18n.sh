916e883cea00da4dc196d76382727007b319c8de
macagua <macagua@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-03-03 15:24:07 +0000

    Added a bash script for generate a new string

---
+++ src/collective/lineage/locales/rebuild_i18n.sh
@@ -0,0 +1,19 @@
+i18ndude rebuild-pot --pot ./collective.lineage.pot --create collective.lineage ../ || exit 1
+i18ndude sync --pot ./collective.lineage.pot ./*/LC_MESSAGES/collective.lineage.po
+
+WARNINGS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-WARN' | wc -l`
+ERRORS=`find . -name "*pt" | xargs i18ndude find-untranslated | grep -e '^-ERROR' | wc -l`
+FATAL=`find . -name "*pt"  | xargs i18ndude find-untranslated | grep -e '^-FATAL' | wc -l`
+
+echo
+echo "There are $WARNINGS warnings \(possibly missing i18n markup\)"
+echo "There are $ERRORS errors \(almost definitely missing i18n markup\)"
+echo "There are $FATAL fatal errors \(template could not be parsed, eg. if it\'s not html\)"
+echo "For more details, run \'find . -name \"\*pt\" \| xargs i18ndude find-untranslated\' or" 
+echo "Look the rebuild i18n log generate for this script called \'rebuild_i18n.log\' on locales dir" 
+
+rm ./rebuild_i18n.log
+
+touch ./rebuild_i18n.log
+
+find ../ -name "*pt" | xargs i18ndude find-untranslated > rebuild_i18n.log
