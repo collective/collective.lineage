a760a64a9f0fe61936a3f05a55639c15dd4a736f
claytron <claytron@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-04-11 13:03:38 +0000

    PEP8 and pyflakes

--- src/collective/lineage/tests.py
+++ src/collective/lineage/tests.py
@@ -1,8 +1,13 @@
+import unittest
 import zope.interface
 import zope.component
+from zope.app.component.interfaces import ISite
+from zope.component import getMultiAdapter
+from zope.component import getUtility
+from zope.interface import alsoProvides
+from zope.interface import noLongerProvides
 from AccessControl import Unauthorized
-from plone.app.layout.navigation.interfaces import INavigationRoot
-
+from Testing import ZopeTestCase as ztc
 #for plone3-plone4 compatibility purposes
 try:
     import zope.app.annotation.attribute as zaa
@@ -12,42 +17,25 @@ try:
     import zope.app.annotation.interfaces as zai
 except:
     import zope.annotation.interfaces as zai
-
-from DateTime import DateTime
 from five.localsitemanager import make_objectmanager_site
-
-from p4a.subtyper import interfaces
-from p4a.subtyper import default
-from p4a.subtyper import engine
-from plone.app.layout.navigation.defaultpage import getDefaultPage
-from plone.portlets.constants import CONTEXT_CATEGORY, GROUP_CATEGORY, CONTENT_TYPE_CATEGORY
+from plone.portlets.constants import CONTEXT_CATEGORY
+from plone.portlets.constants import GROUP_CATEGORY
+from plone.portlets.constants import CONTENT_TYPE_CATEGORY
 from plone.portlets.interfaces import ILocalPortletAssignable
 from plone.portlets.interfaces import ILocalPortletAssignmentManager
 from plone.portlets.interfaces import IPortletManager
 from plone.portlets.interfaces import IPortletType
-
-import Products.Archetypes.interfaces
-from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
-
-from zope.app.component.interfaces import ISite
-
-from Products.CMFCore.utils import getToolByName
-
-from Products.GenericSetup.upgrade import _upgrade_registry
-
-import unittest
 from Products.Five import zcml
 from Products.Five import fiveconfigure
-
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase.layer import onsetup
-from Testing import ZopeTestCase as ztc
+from Products.CMFCore.utils import getToolByName
+import Products.Archetypes.interfaces
+from Products.GenericSetup.upgrade import _upgrade_registry
+from p4a.subtyper import interfaces
+from p4a.subtyper import default
+from p4a.subtyper import engine
 
-from zope.app.component.interfaces import ISite
-from zope.component import getMultiAdapter
-from zope.component import getUtility
-from zope.interface import alsoProvides
-from zope.interface import noLongerProvides
 
 @onsetup
 def setup_package():
@@ -57,8 +45,10 @@ def setup_package():
     fiveconfigure.debug_mode = False
     ztc.installPackage('collective.lineage')
 
+
 setup_package()
 ptc.setupPloneSite(products=['collective.lineage'])
+
 
 class IntegrationTests(ptc.PloneTestCase):
 
@@ -71,9 +61,12 @@ class IntegrationTests(ptc.PloneTestCase):
     def test_folder_is_activatable(self):
         zope.component.provideAdapter(zaa.AttributeAnnotations)
         zope.component.provideAdapter(default.folderish_possible_descriptors)
+
         class SimpleFolder(object):
-            zope.interface.implements(Products.Archetypes.interfaces.IBaseFolder)
+            zope.interface.implements(
+                Products.Archetypes.interfaces.IBaseFolder)
             portal_type = 'Folder'
+
         zope.interface.classImplements(SimpleFolder,
                                        zai.IAttributeAnnotatable)
 
@@ -86,7 +79,9 @@ class IntegrationTests(ptc.PloneTestCase):
         self.portal.invokeFactory('Folder', 'site1')
         zope.component.provideUtility(engine.Subtyper())
         subtyper = zope.component.getUtility(interfaces.ISubtyper)
-        subtyper.change_type(self.portal.site1, u'collective.lineage.childsite')
+        subtyper.change_type(
+            self.portal.site1,
+            u'collective.lineage.childsite')
         self.failUnless(ISite.providedBy(self.portal.site1))
 
     def test_uninstall(self):
@@ -101,8 +96,10 @@ class IntegrationTests(ptc.PloneTestCase):
 
         zope.component.provideAdapter(zaa.AttributeAnnotations)
         zope.component.provideAdapter(default.folderish_possible_descriptors)
+
         class SimpleFolder(object):
-            zope.interface.implements(Products.Archetypes.interfaces.IBaseFolder)
+            zope.interface.implements(
+                Products.Archetypes.interfaces.IBaseFolder)
             portal_type = 'Folder'
 
         zope.interface.classImplements(SimpleFolder,
@@ -111,6 +108,7 @@ class IntegrationTests(ptc.PloneTestCase):
         adapted = interfaces.IPossibleDescriptors(SimpleFolder())
         self.failUnless(u'collective.lineage.childsite' not in \
                             dict(adapted.possible).keys())
+
 
 class MigrationTests(ptc.PloneTestCase):
     """we are going to test the migration from 0.1 to >0.1"""
@@ -132,9 +130,9 @@ class MigrationTests(ptc.PloneTestCase):
 
         self.pw = getToolByName(self.portal, "portal_workflow")
 
-
     def run_migration_step(self):
-        import transaction; transaction.savepoint();
+        import transaction
+        transaction.savepoint()
         # Now run the migration step
         profile_id = "collective.lineage:default"
         setup_tool = getToolByName(self.portal, 'portal_setup')
@@ -168,7 +166,6 @@ class MigrationTests(ptc.PloneTestCase):
         # cf2
         self.portal.invokeFactory("Child Folder", "cf2")
         cf2 = self.portal.cf2
-
 
         #cf3
         cf2.invokeFactory("Child Folder", "cf3")
@@ -243,7 +240,8 @@ class MigrationTests(ptc.PloneTestCase):
         self.failUnless(cf1.Title() == "CF 1")
         self.failUnless(self.pw.getInfoFor(cf1, "review_state") == "published")
         self.failUnless(ISite.providedBy(cf1))
-        # Child folders in 0.1 seemed to provide ILocalPortletAssignable but not in 0.6
+        # Child folders in 0.1 seemed to provide ILocalPortletAssignable but
+        # not in 0.6
         if not ILocalPortletAssignable.providedBy(cf1):
             alsoProvides(cf1, ILocalPortletAssignable)
             added_portlet_assignable_interace = True
@@ -255,11 +253,18 @@ class MigrationTests(ptc.PloneTestCase):
             del mapping[m]
         portlet = getUtility(IPortletType, name='plone.portlet.static.Static')
         addview = mapping.restrictedTraverse('+/' + portlet.addview)
-        addview.createAndAdd(data={'header' : u"test title", 'text' : u"test text"})
+        addview.createAndAdd(data={
+            'header': u"test title",
+            'text': u"test text"})
         self.assertEquals(len(mapping), 1)
 
-        left_col_manager = getUtility(IPortletManager, name='plone.leftcolumn', context=cf1)
-        assignment_manager = getMultiAdapter((cf1, left_col_manager), ILocalPortletAssignmentManager)
+        left_col_manager = getUtility(
+            IPortletManager,
+            name='plone.leftcolumn',
+            context=cf1)
+        assignment_manager = getMultiAdapter(
+            (cf1, left_col_manager),
+            ILocalPortletAssignmentManager)
         assignment_manager.setBlacklistStatus(GROUP_CATEGORY, None)
         assignment_manager.setBlacklistStatus(CONTENT_TYPE_CATEGORY, False)
 
@@ -272,11 +277,19 @@ class MigrationTests(ptc.PloneTestCase):
         mapping = cf1.restrictedTraverse('++contextportlets++plone.leftcolumn')
         self.assertEquals(len(mapping), 1)
 
-        left_col_manager = getUtility(IPortletManager, name='plone.leftcolumn', context=cf1)
-        assignment_manager = getMultiAdapter((cf1, left_col_manager), ILocalPortletAssignmentManager)
-        self.assertTrue(assignment_manager.getBlacklistStatus(CONTEXT_CATEGORY))
-        self.assertTrue(assignment_manager.getBlacklistStatus(GROUP_CATEGORY) is None)
-        self.assertFalse(assignment_manager.getBlacklistStatus(CONTENT_TYPE_CATEGORY))
+        left_col_manager = getUtility(
+            IPortletManager,
+            name='plone.leftcolumn',
+            context=cf1)
+        assignment_manager = getMultiAdapter(
+            (cf1, left_col_manager),
+            ILocalPortletAssignmentManager)
+        self.assertTrue(
+            assignment_manager.getBlacklistStatus(CONTEXT_CATEGORY))
+        self.assertTrue(
+            assignment_manager.getBlacklistStatus(GROUP_CATEGORY) is None)
+        self.assertFalse(
+            assignment_manager.getBlacklistStatus(CONTENT_TYPE_CATEGORY))
 
     def test_migration_preserves_references(self):
         self.portal.invokeFactory("Child Folder", "cf1")
@@ -292,19 +305,22 @@ class MigrationTests(ptc.PloneTestCase):
         doc1 = cf1["doc1"]
         cf1.setDefaultPage("doc1")
 
-        doc2_text = '<p><a href="resolveuid/%s" class="internal">Link to doc 1</a></p>' % doc1.UID()
+        doc2_text = (
+            '<p><a href="resolveuid/%s" class="internal">Link to doc 1'
+            '</a></p>') % doc1.UID()
         cf1.invokeFactory("Document", "doc2", Title="Doc 2", text=doc2_text)
         doc2 = cf1["doc2"]
         # I'm not sure what layer of Plone code adds these
         doc2.addReference(doc1, 'isReferencing', updateReferences=True)
-        self.assertEquals(len(doc2._getReferenceAnnotations().objectItems()), 1)
+        self.assertEquals(
+            len(doc2._getReferenceAnnotations().objectItems()), 1)
 
         self.run_migration_step()
 
         cf1 = self.portal.cf1
         doc2 = cf1["doc2"]
-        self.assertEquals(len(doc2._getReferenceAnnotations().objectItems()), 1)
-
+        self.assertEquals(
+            len(doc2._getReferenceAnnotations().objectItems()), 1)
 
     def test_migration_preserves_sharing_settings(self):
         self.portal.invokeFactory("Child Folder", "cf1")
@@ -321,11 +337,12 @@ class MigrationTests(ptc.PloneTestCase):
         self.run_migration_step()
 
         cf1 = self.portal.cf1
-        self.assertEquals(('Contributor',), cf1.get_local_roles_for_userid('testuser'))
+        self.assertEquals(
+            ('Contributor',), cf1.get_local_roles_for_userid('testuser'))
+
 
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(IntegrationTests))
     suite.addTest(unittest.makeSuite(MigrationTests))
     return suite
-
