9bbc365db5eb0c6d77a6f364f105f233023cbbe1
lucie <lucie@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-04-11 14:12:15 +0000

    Divided the subscriber in multiple pieces so the code can be reused outside of lineage

--- src/collective/lineage/subscribers.py
+++ src/collective/lineage/subscribers.py
@@ -9,23 +9,36 @@ from collective.lineage.interfaces import IChildSite
 from collective.lineage.events import ChildSiteCreatedEvent
 from collective.lineage.events import ChildSiteRemovedEvent
 
+def reindexObjectProvides(folder):
+    pc = getToolByName(folder, 'portal_catalog')
+    pc.reindexObject(
+        folder,
+        idxs=['object_provides']
+    )
+
+def enableFolder(folder):
+    if not ISite.providedBy(folder):
+        make_objectmanager_site(folder)
+    # reindex so that the object_provides index is aware of our
+    # new interface
+    reindexObjectProvides(folder)
+    zope.event.notify(ChildSiteCreatedEvent(folder))
+
+def disableFolder(folder):
+    # remove local site components
+    disableSite(folder)
+
+    # reindex the object so that the object_provides index is
+    # aware that we've removed it
+    reindexObjectProvides(folder)
+    zope.event.notify(ChildSiteRemovedEvent(folder))
 
 def enableChildSite(event):
     """When a lineage folder is created, turn it into a component site
     """
     if event.subtype.type_interface == IChildSite:
         folder = event.object
-        if not ISite.providedBy(folder):
-            make_objectmanager_site(folder)
-        # reindex so that the object_provides index is aware of our
-        # new interface
-        pc = getToolByName(folder, 'portal_catalog')
-        pc.reindexObject(
-            folder,
-            idxs=['object_provides'],
-            )
-        zope.event.notify(ChildSiteCreatedEvent(event.object))
-
+        enableFolder(folder)
 
 def disableChildSite(event):
     """When a child site is turned off, remove the local components
@@ -33,14 +46,4 @@ def disableChildSite(event):
     if event.subtype is not None and \
       event.subtype.type_interface == IChildSite:
         folder = event.object
-        # remove local site components
-        disableSite(folder)
-
-        # reindex the object so that the object_provides index is
-        # aware that we've removed it
-        pc = getToolByName(folder, 'portal_catalog')
-        pc.reindexObject(
-            folder,
-            idxs=['object_provides'],
-            )
-        zope.event.notify(ChildSiteRemovedEvent(event.object))
+        disableFolder(folder)
