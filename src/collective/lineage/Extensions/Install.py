1668d2effd5949016775c86b503b87a5a87a474c
Luca Fabbri <luca@keul.it>
2013-01-03 18:11:32 +0100

    when uninstall, remove the IChildSite interface from all objects marked as subsite

--- src/collective/lineage/Extensions/Install.py
+++ src/collective/lineage/Extensions/Install.py
@@ -2,6 +2,10 @@ from cStringIO import StringIO
 from Products.CMFCore.utils import getToolByName
 
 from p4a.subtyper.interfaces import IFolderishContentTypeDescriptor
+from p4a.subtyper.interfaces import ISubtyped
+from p4a.z2utils.utils import remove_marker_ifaces
+
+from collective.lineage.interfaces import IChildSite
 
 def runProfile(portal, profileName):
     setupTool = getToolByName(portal, 'portal_setup')
@@ -27,6 +31,7 @@ def uninstall(portal, reinstall=False):
     if not reinstall:
         runProfile(portal, 'profile-collective.lineage:uninstall')
         _unregisterUtility(portal)
+        remove_marker_ifaces(portal, (IChildSite, ISubtyped))
         print >> out, "Uninstalled collective.lineage"
     
     return out.getvalue()
