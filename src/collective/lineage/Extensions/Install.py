b39877dc99842e2d72f1ab4b62dfd12618aa6e71
Luca Fabbri <luca@keul.it>
2013-01-03 17:37:24 +0100

    Uninstall will now remove other stuff

--- src/collective/lineage/Extensions/Install.py
+++ src/collective/lineage/Extensions/Install.py
@@ -1,6 +1,7 @@
 from cStringIO import StringIO
 from Products.CMFCore.utils import getToolByName
 
+from p4a.subtyper.interfaces import IFolderishContentTypeDescriptor
 
 def runProfile(portal, profileName):
     setupTool = getToolByName(portal, 'portal_setup')
@@ -14,11 +15,18 @@ def install(portal):
     print >> out, "Installed collective.lineage"
     return out.getvalue()
 
+def _unregisterUtility(portal):
+    sm = portal.getSiteManager()
+    util = sm.queryUtility(IFolderishContentTypeDescriptor, u'collective.lineage.childsite')
+    sm.unregisterUtility(util, IFolderishContentTypeDescriptor, name=u'collective.lineage.childsite')
+    del sm.utilities._subscribers[0][IFolderishContentTypeDescriptor]
 
 def uninstall(portal, reinstall=False):
     """Run the GS profile to install this package"""
     out = StringIO()
     if not reinstall:
         runProfile(portal, 'profile-collective.lineage:uninstall')
+        _unregisterUtility(portal)
         print >> out, "Uninstalled collective.lineage"
+    
     return out.getvalue()
