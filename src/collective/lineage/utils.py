deeb2ce9ddf77a8748a6354797f5cea28f6e03b4
Johannes Raggam <thetetet@gmail.com>
2016-04-24 00:25:38 +0200

    Implement new utility method ``parent_site``.

--- src/collective/lineage/utils.py
+++ src/collective/lineage/utils.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
+from Acquisition import aq_parent
 from collective.lineage.events import ChildSiteCreatedEvent
 from collective.lineage.events import ChildSiteRemovedEvent
 from collective.lineage.events import ChildSiteWillBeCreatedEvent
 from collective.lineage.events import ChildSiteWillBeRemovedEvent
 from collective.lineage.interfaces import IChildSite
 from five.localsitemanager import make_objectmanager_site
+from Products.CMFPlone.interfaces import IPloneSiteRoot
 from Products.Five.component import disableSite
+from zope.component.hooks import getSite
 from zope.component.interfaces import ISite
 from zope.event import notify
 from zope.interface import alsoProvides
@@ -37,3 +40,20 @@ def disable_childsite(context):
 
     context.reindexObject(idxs=('object_provides'))
     notify(ChildSiteRemovedEvent(context))
+
+
+def parent_site():
+
+    def _find_site(ctx):
+        # First, stop at IPloneSiteRoot
+        if IPloneSiteRoot.providedBy(ctx):
+            return ctx
+        # Then walk up
+        ctx = aq_parent(ctx)
+        if IChildSite.providedBy(ctx):
+            return ctx
+        return _find_site(ctx)
+
+    # Start at current site
+    site = _find_site(getSite())
+    return site
