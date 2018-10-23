17eabbb815a9a809bab909b333ee234996c1d3fe
Ross Patterson <me@rpatterson.net>
2018-10-23 15:24:53 -0700

    Update import location

--- src/collective/lineage/adapters.py
+++ src/collective/lineage/adapters.py
@@ -1,9 +1,20 @@
 # -*- coding: utf-8 -*-
 from Products.CMFPlone.interfaces import IPloneSiteRoot
 from zope.component import queryUtility
-from plone.app.imaging.interfaces import IImagingSchema
+from plone.app.imaging import scaling
+
+from plone.browserlayer import layer
+
+
+def mark_layer(site, event):
+    """
+    Support re-marking an already marked request.
+    """
+    if hasattr(event.request, "_plonebrowserlayer_"):
+        del event.request._plonebrowserlayer_
+    return layer.mark_layer(site, event)
 
 
 def get_image_traverser(context):
     siteroot = queryUtility(IPloneSiteRoot)
-    return IImagingSchema(siteroot)
+    return scaling.IImagingSchema(siteroot)
