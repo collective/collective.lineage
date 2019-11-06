a681b3e2657912660cc0f56ece50a6ed16a2566c
Ross Patterson <me@rpatterson.net>
2019-11-05 22:43:34 -0800

    Python3: Fix deprecation warning

--- src/collective/lineage/events.py
+++ src/collective/lineage/events.py
@@ -3,7 +3,8 @@ from collective.lineage.interfaces import IChildSiteCreatedEvent
 from collective.lineage.interfaces import IChildSiteRemovedEvent
 from collective.lineage.interfaces import IChildSiteWillBeCreatedEvent
 from collective.lineage.interfaces import IChildSiteWillBeRemovedEvent
-from zope.component.interfaces import ObjectEvent
+
+from zope.interface.interfaces import ObjectEvent
 from zope.interface import implementer
 
 
